dcdeb11f179c9b969d5f3c6bcc0673da58116f90
ggabunia <ggabunia91@outlook.com>
2019-11-26 12:11:04 +0400

    added observables to services

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from "../shared/ingredient-model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,21 +9,22 @@ import {ShoppingListService} from "./shopping-list.service";
   styleUrls: ['./shopping-list.component.css'],
   providers: []
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  //   = [
-  //   new Ingredient('Apples',5),
-  //   new Ingredient('Tomatoes',10),
-  // ];
+  private subscription: Subscription
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
    this.ingredients = this.shoppingListService.getIngredients();
-   this.shoppingListService.ingredientsChanged.subscribe(
+   this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 
